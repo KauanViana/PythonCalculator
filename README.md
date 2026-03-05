9f72a98eb4ae217f4c596cdde90e5e585276ea44
Kauan Viana <kauanviana47727@gmail.com>
2026-03-04 22:20:17 -0300

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # PythonCalculator
 
+
+
+## Eng-US
+A text-based calculator built with Python. The goal is to create a program where the user inputs a low or medium-complexity operation in a single line, and the code returns the result.
+
+## Pt-BR
+Calculadora baseada em texto desenvolvida em Python. O objetivo é criar um programa no qual o usuário digita, em uma única linha, uma operação de complexidade baixa ou média, e o código retorna o seu resultado.
---
+++ index.py
@@ -0,0 +1,57 @@
+def calc(initial_value, additional_value, operator):
+    if operator == '+':
+        return initial_value + additional_value   
+    elif operator == '-':
+        return initial_value - additional_value   
+    elif operator == '*':
+        return initial_value * additional_value   
+    elif operator == '/':
+        return initial_value / additional_value   
+
+
+print('Calculadora no Terminal\n')
+user_input = input('=> ').replace(' ', '')
+values = []
+operators = []
+sub_result = 0
+sub_results = []
+
+value_index = 0
+for i in user_input:
+    if i not in ['+', '-']:
+        values.append('')
+        values[value_index] += i
+    else:
+        value_index += 1
+        operators.append(i)
+    if '' in values:
+        del values[-1]
+
+for i in values:
+    value_index = 0
+    sub_values = []
+    sub_operators = []
+    for y in range(len(i)):
+        if i[y] not in ['*', '/']:
+            sub_values.append('')
+            sub_values[value_index] += i[y]
+        else:
+            value_index += 1
+            sub_operators.append(i[y])
+        if '' in sub_values:
+            del sub_values[-1]
+    sub_result = sub_values[0]
+    for y in range(len(sub_operators)):
+        sub_result = calc(float(sub_result), float(sub_values[y+1]), sub_operators[y])
+    sub_results.append(sub_result)
+
+for i in range(len(values)):
+    values[i] = sub_results[i]
+
+
+result = values[0]
+for i in range(len(operators)):
+    result = calc(float(result), float(values[i+1]), operators[i])
+
+
+print(f'O resultado é: {float(result)}')
